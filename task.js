e062739574f34a2cd4b687e36edea3a6a030ac16
Igor Evdokimov <evdokimov048@gmail.com>
2022-05-12 07:15:22 +0300

    tasks ready

--- task.js
+++ task.js
@@ -1,32 +1,36 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  let discriminant = b**2 - 4 * a * c;
+  let arr = [];
+  const discriminant = b**2 - 4 * a * c;
   let discriminantRootX;
   let discriminantRootY;
 
-  switch (discriminant) {
-    case discriminant < 0:
-      arr = [];
-      break;
-      
-    case discriminant === 0:
-      discriminantRootX = -b / (2*a); 
-      arr = [discriminantRootX];
-      break;
-
-    case discriminant > 0:
-      discriminantRootX = (-b + Math.sqrt(d)) / (2 * a);
-      discriminantRootY = (-b - Math.sqrt(d)) / (2 * a);
-      arr = [discriminantRootX, discriminantRootY];
-      break;  
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    discriminantRootX = -b / (2 * a);
+    arr.push(discriminantRootX);
+  } else {
+    discriminantRootX = (-b + Math.sqrt(discriminant) ) / (2 * a);
+    discriminantRootY = (-b - Math.sqrt(discriminant) ) / (2 * a);
+    arr.push(discriminantRootX, discriminantRootY);
   }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+
   let totalAmount;
+  let creditBody = amount - contribution;
+  let monthlyPercent = percent / 12 / 100;
+  let presentDate = new Date();
+  let monthsInMileseconds = date.getTime() - presentDate.getTime()
+  let monthsToPay = Math.floor(monthsInMileseconds / 2592000000);
+    
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**monthsToPay) - 1)));
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  totalAmount = monthlyPayment * monthsToPay;
+  console.log(+(totalAmount.toFixed(2)));
+  return +(totalAmount.toFixed(2));
 }
