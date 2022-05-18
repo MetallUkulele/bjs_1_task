22359cc797fe9e250d31bb106e97543868bbdeb9
Igor Evdokimov <evdokimov048@gmail.com>
2022-05-18 16:13:52 +0300

    Исправлена валидация

--- task.js
+++ task.js
@@ -4,23 +4,34 @@ function solveEquation(a, b, c) {
   let arr = [];
   const discriminant = b**2 - 4 * a * c;
   let discriminantRootX;
-  let discriminantRootY;
+  let discriminantRootY; 
 
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant === 0) {
-    discriminantRootX = -b / (2 * a);
-    arr.push(discriminantRootX);
-  } else {
+  if (discriminant > 0) {
     discriminantRootX = (-b + Math.sqrt(discriminant) ) / (2 * a);
     discriminantRootY = (-b - Math.sqrt(discriminant) ) / (2 * a);
     arr.push(discriminantRootX, discriminantRootY);
-  }
+  } else if (discriminant === 0) {
+    discriminantRootX = -b / (2 * a);
+    arr.push((-b / (2 * a)));
+  } 
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
+  if (typeof percent !== 'number') {    
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+
+  if (typeof contribution !== 'number') {    
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+
+  if (typeof amount !== 'number') {    
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  
   let totalAmount;
   let creditBody = amount - contribution;
   let monthlyPercent = percent / 12 / 100;
@@ -32,5 +43,6 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   totalAmount = monthlyPayment * monthsToPay;
   console.log(+(totalAmount.toFixed(2)));
+  
   return +(totalAmount.toFixed(2));
 }
